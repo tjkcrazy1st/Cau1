243c69942ba231c308460e7c6da80de7e4c4a692
Lê Hoàng <windcross451998@gmail.com>
2019-08-20 13:55:59 +0700

    Cau 1

---
+++ MyApplication/app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.student.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txt;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.textView2);
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                EditText text = (EditText) findViewById(R.id.editText);
+                final String value = text.getText().toString();
+                txt.setText("Xin chào: " + value);
+            }
+        });
+    }
+}
